7605135e880cad311b48923205df3ebe6d5c5c20
arshad <arshad@thinkebiz.net>
2023-05-17 14:39:56 +0530

    Login Page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from "react";
+import  "./Login.css";
+import profile from "../../images/a.png";
+import email from "../../images/email.jpg";
+import pass from "../../images/pass.png";
 
-const Login =() =>{
+const Login = () => {
   return (
-    <div>
-
-        <h1>LOGIN</h1>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input type="text" placeholder="user name" className="name" />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email" />
+              <input type="password" placeholder="user name" className="name" />
+            </div>
+            <div className="login-button">
+              <button>Login</button>
+            </div>
+            <p className="link">
+              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
