14b9e6d7694fa19ac08af7ec8356b23b76732e5f
arshad <arshad@thinkebiz.net>
2023-06-23 18:44:55 +0530

    Add

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,15 @@
 import React from "react";
 import  "./Login.css";
-import profile from "../../images/img1.jpg";
-// import profile from "../../images/pr.png";
+import profile from "../../images/mypic.jpg";
 import email from "../../images/email.jpg";
 import pass from "../../images/pass.png";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleOnClick = ()=>{
+    navigate("/Signup")
+  }
   return (
     <div className="main">
       <div className="sub-main">
@@ -19,17 +23,18 @@ const Login = () => {
             <h1>Login Page</h1>
             <div>
               <img src={email} alt="email" className="email" />
-              <input type="text" placeholder="user name" className="name" />
+              <input type="text" placeholder="Please enter email" className="name" />
             </div>
             <div className="second-input">
               <img src={pass} alt="pass" className="email" />
-              <input type="password" placeholder="user name" className="name" />
+              <input type="password" placeholder="Please enter password" className="name" />
             </div>
             <div className="login-button">
               <button>Login</button>
             </div>
             <p className="link">
-              <a href="#">Forgot password ?</a> Or <a href="#">Sign Up</a>
+              {/* <a href="#">Forgot password ?</a> Or  */}
+               <a onClick={(e)=>handleOnClick(e)}> Sign Up</a>
             </p>
           </div>
         </div>
